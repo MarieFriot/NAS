200d1d55afb3b4990c5dff250c762954d4ee3bab
MarieFriot <154432198+MarieFriot@users.noreply.github.com>
2024-03-30 23:24:48 +0100

    Add files via upload

---
+++ libgns3.py
@@ -0,0 +1,442 @@
+import time
+import json
+
+
+
+
+
+def ip_address(name,neighbor):
+    """
+    Parameters
+    ----------
+    name : Int : numéro de routeur
+    neighbor : Int : numéro du routeur voisin
+    Returns
+    -------
+    res : int :valeur correspondant au sous-réseau entre les deux routeurs
+    Cette valeur est utilisée pur configurer leurs adresses ip.
+    """
+    if neighbor == "EXIT" :
+        res = 1
+    elif name < neighbor :
+        res = name*10 + neighbor
+    else :
+        res = neighbor*10 + name
+    return res 
+
+def setNeighborVal(neighborType) :
+    """
+    Parameters
+    ----------
+    neighborType : Chaine de caractère (peer, provider ou customer)
+    Returns
+    -------
+    res : Entier
+        Retourne un entier en fonction du type de relation avec l'AS voisine.
+        Cette entier est utilisé pour attribuer la localPref ainsi que les numéros
+        de communauté.
+    """
+    if  neighborType == "customer" :
+         res = 500
+    elif  neighborType == "peer":
+         res = 300
+    elif  neighborType == "provider":
+         res = 100
+    else :
+         res = 0
+    return res
+
+def write(lastConfig, key, com, lvl, indice, tn) :
+    """
+    Parameters
+    ----------
+    lastConfig : Dictionnaire contenant la dernière configuration du routeur
+    key : Clef du dictionnaire lastConfig.
+    com : String : Commande que l'on veut rentrer.
+    lvl : Si lvl == 0, la commande est modifiable, si non, on ne peut pas la
+        modifier (ex: configure terminal)
+    indice : Int : Correspond à l'indice de la liste contenant la suite des
+        anciennes commandes.
+    tn : telnet
+    -------
+    Fonction vérifiant si la nouvelle commande est différente de la précédente.
+    Si la commande est différente : on applique "no" devant l'ancienne et on 
+    l'écrit sur le routeur pour la supprimer.
+    Après avoir écrit la nouvelle commande sur le routeur, on la garde en mémoire
+    dans le dictionnaire de LastConfig.
+    """
+    
+    if lvl == 0 : #si la commande est changeable
+        if key in lastConfig.keys() :
+            if indice <= len(lastConfig[key])-1: #important pour la premiere config
+                lastCom = lastConfig[key][indice]
+                if com != lastCom: #Si la commande est différente de la précédente
+                    if lastCom[0] == "n" and lastCom[1] == "o":
+                        noCom = lastCom[2:]
+                    else :
+                        noCom = "no " + lastCom
+                    #print(noCom)
+
+                    tn.write(noCom.encode('utf-8')) #On enleve la commande précédente
+                    tn.read_until(b"#")  
+    print(com)
+    tn.write(com.encode('utf-8')) #On écrit la nouvelle commande 
+    tn.read_until(b"#")
+    
+    #On ajoute la nouvelle commande dans le fichier qui garde en mémoire les commandes entrées 
+    if key not in lastConfig.keys() :
+        lastConfig[key] = [com] 
+    elif len(lastConfig[key]) -1 < indice : #important si l'indice dépasse la taille de la liste
+        lastConfig[key].append(com) 
+    else :
+        lastConfig[key][indice] = com 
+  
+  
+
+
+
+
+def interfaceConfig(name, conf, commande,tn):
+    """
+    Parameters
+    ----------
+    name: Chaine de caractère : nom du routeur 
+    conf : Dictionnaire contenant la configuration de chaques routeur du réseau.
+    commande : Dictionnaire contenant les commandes de configuration d'un routeur cisco.
+    tn : telnetlib.Telnet
+    Returns
+    -------
+    Configure chaques interface du routeur.
+    """
+    compteurRd = name*10 
+        
+ 
+    with open(f"lastConfig/lastConfig{name}.json","r") as f:
+        lastConfig = json.load(f)
+        
+   
+    AS = conf['as']
+    neighbors = conf['neighbors']
+    
+    
+    for interface, neighbor in neighbors.items() :
+        if interface == "loopback" :
+            masque = "255.255.255.255"
+        
+        else :
+            masque = "255.255.255.252"
+            
+            
+      
+        if neighbor[0] != "NULL" :
+            ip_val = ip_address(name, neighbor[0])
+    
+            ######Configuration des vrfs#################
+           
+            if neighbor[1] != AS and neighbor[2] != "passive" :
+                numClient = neighbor[2]
+                for i in range(len(commande['vrf']['config'])):
+                    comDico = commande['vrf']['config'][i]
+                    for com, lvl in comDico.items():
+                        com= com.format(numClient = numClient, compteur = compteurRd, AS = AS, asClient = neighbor[1])
+                        compteurRd = compteurRd +1
+                        if 'vrf' not in lastConfig.keys():
+                            lastConfig['vrf'] = {}
+                        write(lastConfig['vrf'], numClient ,com, lvl, i, tn)
+                        
+                importRT = conf['import']
+                if len(importRT) > 0:
+                    for infoClient in importRT :
+                        for j in range(len(commande['vrf']['importRT'])):
+                            if infoClient[0] == numClient : #on vérifie qu'on est sur la bonne vrf
+                                i = i+1
+                                comDico = commande['vrf']['importRT'][j]
+                                for com, lvl in comDico.items():
+                                    com= com.format(numClient = infoClient[1] , asClient = infoClient[2])
+                                    write(lastConfig['vrf'], numClient ,com, lvl, i, tn)
+                     
+                #Si la nouvelle suite de commande est plus petite que la précédente
+                if i < len(lastConfig['vrf'][numClient])  :
+                     for j in range(i +1, len(lastConfig['vrf'][numClient])):
+                         del lastConfig['vrf'][numClient][i+1] 
+                
+                
+                
+            ##### Configuration des interfaces ###########
+
+            ## Ecriture de commande obligatoire pour accéder aux interfaces
+            for i in range(len(commande['interface'][interface])):
+                if neighbor[1] != AS and i == 0  and neighbor[2] != "passive":
+                    pass
+                else :
+                    comDico = commande['interface'][interface][i]
+                    for com, _ in comDico.items():
+                        tn.write(com.encode('utf-8'))
+                        tn.read_until(b"#")
+            
+            numClient = 0
+            if conf['routerType'] == "CE"  or conf['routerType'] == "CEIP":
+                interfaceType = "noIGP"
+            elif neighbor[1] == AS :
+                interfaceType =  conf['routing_protocol']
+            elif neighbor[2] == "passive" :
+                interfaceType = "passive"
+            else :
+                interfaceType = "vrf"
+                numClient = neighbor[2]
+         
+            for i in range(len(commande['interface'][interfaceType])):
+                comDico = commande['interface'][interfaceType][i]
+                for com, lvl in comDico.items():
+                    com= com.format(name= name%2 +1,numClient = numClient, ip_val = ip_val, AS = neighbor[1], masque = masque, interface = interface)
+                    if 'interface' not in lastConfig.keys():
+                        lastConfig['interface'] = {}
+                    write(lastConfig['interface'], interface ,com, lvl, i, tn)
+            
+            tn.write(b"end\r") 
+            tn.read_until(b"#")
+            #Si la nouvelle suite de commande est plus petite que la précédente
+            if i < len(lastConfig['interface'][interface])  :
+                for j in range(i+1, len(lastConfig['interface'][interface])):
+                    del lastConfig['interface'][interface][i+1]
+         
+            
+    with open(f"lastConfig/lastConfig{name}.json","w") as f:
+        json.dump(lastConfig, f)
+   
+
+
+                    
+def labelProtocolConfig(name, conf, commande,tn):
+
+    with open(f"lastConfig/lastConfig{name}.json","r") as f:
+        lastConfig = json.load(f)
+        
+    if conf['routerType'] != "CE" and conf['routerType'] != "CEIP":
+        ######LDP SUR ROUTEUR##################
+        for i in range(len(commande['labelProtocol']['ldp'])):
+                comDico = commande['labelProtocol']['ldp'][i]
+                for com, lvl in comDico.items():
+                    if 'labelProtocol' not in lastConfig.keys():
+                        lastConfig['labelProtocol'] = {}
+                    write(lastConfig['labelProtocol'], 'router' ,com, lvl, i, tn)
+                    
+        tn.write(b"end\r") 
+        tn.read_until(b"#")
+        
+        #Si la nouvelle suite de commande est plus petite que la précédente
+        if i < len(lastConfig['labelProtocol']['router'])  :
+            for j in range(i+1, len(lastConfig['labelProtocol']['router'])):
+                del lastConfig['labelProtocol']['router'][i+1] 
+        
+        time.sleep(1)
+        ##############LDP SUR INTERFACE##################
+        
+        neighbors = conf['neighbors']
+        AS = conf['as']
+        for interface, neighbor in neighbors.items() :
+            if interface != "loopback" and neighbor[1] == AS :  #pas sur loopback ni sur interface non dans coeur
+                if neighbor[0] != "NULL"  : 
+                    ## Ecriture de commande obligatoire pour accéder aux interfaces
+                    for i in range(len(commande['interface'][interface])):
+                        comDico = commande['interface'][interface][i]
+                        for com, _ in comDico.items():
+                            tn.write(com.encode('utf-8'))
+                            tn.read_until(b"#")
+                
+                    for i in range(len(commande['labelProtocol']['ldpInterface'])):
+                            comDico = commande['labelProtocol']['ldpInterface'][i]
+                            
+                            for com, lvl in comDico.items():
+                                if 'labelProtocol' not in lastConfig.keys():
+                                    lastConfig['labelProtocol'] = {}
+                                write(lastConfig['labelProtocol'], interface ,com, lvl, i, tn)
+                                
+                    tn.write(b"end\r") 
+                    tn.read_until(b"#")
+                  
+                    
+                    #Si la nouvelle suite de commande est plus petite que la précédente
+                    if i < len(lastConfig['labelProtocol'][interface])  :
+                      
+                        for j in range(i+1, len(lastConfig['labelProtocol'][interface])):
+                            del lastConfig['labelProtocol'][interface][i+1] 
+    
+                 
+    with open(f"lastConfig/lastConfig{name}.json","w") as f:
+        json.dump(lastConfig, f)
+    
+     
+
+def bgpConfig (name, conf, commande,tn) :
+    """
+    Parameters
+    ----------
+    name : Chaine de caractère : nom du routeur 
+    conf : Dictionnaire contenant la configuration de chaques routeur du réseau.
+    commande : Dictionnaire contenant les commandes de configuration d'un routeur cisco.
+    tn : telnetlib.Telnet
+    Returns
+    -------
+    Configure bgp sur le routeur.
+    """
+    
+   
+    with open(f"lastConfig/lastConfig{name}.json","r") as f:
+        lastConfig = json.load(f)
+        
+    AS = conf['as']
+    routerType = conf['routerType']
+    neighbors = conf['neighbors']
+    
+    
+    indice = 0
+    
+    #ecriture des commandes obligatoires pour BGP
+    for i in range(len(commande['bgp']['config'])):
+        comDico = commande['bgp']['config'][i]
+        for com, lvl in comDico.items():
+            com = com.format(name=name, AS = AS)
+            write(lastConfig, 'bgp',com, lvl, indice, tn)
+        indice +=1
+          
+    if routerType != "CEIP" :
+   
+        if routerType == "PE" :
+            PErouter = conf['PErouter']
+            for router in PErouter :
+                ip_val = ip_address(router, router)
+                #configuration des sessions iBGP
+                for i in range(len(commande['bgp']['internalSession'])) :
+                    comDico = commande['bgp']['internalSession'][i]
+                    for com, lvl in comDico.items():
+                        com= com.format(namePe=router%2 +1, AS = AS, ip_val = ip_val)
+                        write(lastConfig, 'bgp',com, lvl, indice, tn)
+                    indice +=1
+                time.sleep(1)
+                
+            #configuration des sessions eBGP
+            CErouter = conf['CErouter']
+            for router in CErouter :
+                ip_val = ip_address(router[0], name)
+                for i in range(len(commande['bgp']['externalSessionPE'])) :
+                    comDico = commande['bgp']['externalSessionPE'][i]
+                    for com, lvl in comDico.items():
+                        com= com.format(name=router[0]%2 +1, ASNeighbor = router[1] , ip_val = ip_val, numClient = router[2])
+                        write(lastConfig, 'bgp',com, lvl, indice, tn)
+                    indice +=1
+            time.sleep(1)
+                
+                
+        ##Configuration des sessions bgp sur les CE
+        if routerType == "CE" :
+            PErouter = conf['PErouter']
+            for router in PErouter :
+                ip_val = ip_address(router[0], name)
+                for i in range(len(commande['bgp']['externalSessionCE'])) :
+                    comDico = commande['bgp']['externalSessionCE'][i]
+                    for com, lvl in comDico.items():
+                        com= com.format(name=router[0]%2 +1, AS= AS, ASNeighbor= router[1] , ip_val = ip_val)
+                        write(lastConfig, 'bgp',com, lvl, indice, tn)
+                    indice +=1
+                    
+            for subnet in conf['advertise'] :
+                for i in range(len(commande['bgp']['advertiseCE'])) :
+                    comDico = commande['bgp']['advertiseCE'][i]
+                    for com, lvl in comDico.items():
+                        com= com.format(subnet = subnet, AS = AS)
+                        write(lastConfig, 'bgp',com, lvl, indice, tn)
+                    indice +=1
+            
+            time.sleep(1)
+ 
+                
+    tn.write(b"end\r") 
+    tn.read_until(b"#")
+    #Si la nouvelle suite de commande est plus petite que la précédente
+    if indice < len(lastConfig['bgp'])  :
+        for j in range(indice+1, len(lastConfig['bgp'])):
+            del lastConfig['bgp'][indice+1]   
+            
+    
+    with open(f"lastConfig/lastConfig{name}.json","w") as f:
+        json.dump(lastConfig, f)
+    
+def ipv4BgpConfig(name, conf, commande,tn) :
+    with open(f"lastConfig/lastConfig{name}.json","r") as f:
+        lastConfig = json.load(f)
+        
+    AS = conf['as']
+    routerType = conf['routerType']
+    
+    indice = 0
+    
+    #ecriture des commandes obligatoires pour BGP
+    for i in range(len(commande['bgp']['config'])):
+        comDico = commande['bgp']['config'][i]
+        for com, lvl in comDico.items():
+            com = com.format(name=name, AS = AS)
+            write(lastConfig, 'bgpv4',com, lvl, indice, tn)
+        indice +=1
+   
+    
+    print("ixiiii")
+    if routerType != "CE":
+        
+        ASrouter = conf['ASrouter']
+        #Configuragion des sessions iBGP (ipv4) en full-mesh
+        if len(ASrouter) > 0 :
+            for router in ASrouter :
+                ip_val = ip_address(router, router)
+                #configuration des sessions iBGP
+                for i in range(len(commande['bgp']['internalSessionIP'])) :
+                    comDico = commande['bgp']['internalSessionIP'][i]
+                    for com, lvl in comDico.items():
+                        com= com.format(nameR=router%2 +1, AS = AS, ip_val = ip_val)
+                        write(lastConfig, 'bgpv4',com, lvl, indice, tn)
+                    indice +=1
+                time.sleep(1)
+            
+        #configuration des sessions eBGP
+        if routerType != "P" :
+            eBGPRouter = conf['eBGPRouter']
+            if len(eBGPRouter)> 0 :
+                for router in eBGPRouter :
+                    ip_val = ip_address(router[0], name)
+                    for i in range(len(commande['bgp']['externalSessionIP'])) :
+                        comDico = commande['bgp']['externalSessionIP'][i]
+                        if AS == 111 :
+                            ASvalue = router[1] 
+                        else :
+                            ASvalue = AS
+                            
+                        for com, lvl in comDico.items():
+                            com= com.format(name=router[0]%2 +1, ASNeighbor =router[1] , ip_val = ip_val, AS = ASvalue)
+                            
+                            write(lastConfig, 'bgpv4',com, lvl, indice, tn)
+                        indice +=1
+                time.sleep(1)
+                
+        #advertise des CEIP
+        if routerType == "CEIP" :
+            for subnet in conf['advertise'] :
+                for i in range(len(commande['bgp']['advertiseCE'])) :
+                    comDico = commande['bgp']['advertiseCE'][i]
+                    for com, lvl in comDico.items():
+                        com= com.format(subnet = subnet, AS = AS)
+                        write(lastConfig, 'bgpv4',com, lvl, indice, tn)
+                    indice +=1
+        
+                
+                
+    tn.write(b"end\r") 
+    tn.read_until(b"#")
+    #Si la nouvelle suite de commande est plus petite que la précédente
+    if indice < len(lastConfig['bgpv4'])  :
+        for j in range(indice+1, len(lastConfig['bgpv4'])):
+            del lastConfig['bgpv4'][indice+1]   
+            
+    
+    with open(f"lastConfig/lastConfig{name}.json","w") as f:
+        json.dump(lastConfig, f)
+    
---
+++ main.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan  9 14:43:41 2024
+
+@author: mfriot
+"""
+import time
+import telnetlib
+from gns3fy import Gns3Connector, Project,Node
+import json
+import libgns3
+
+
+def portRouteur(nameRouteur, projectName, projectID):
+    """
+    Parameters
+    ----------
+    nameRouteur : String : numéro du routeur
+    projectName : String : nom du projet GNS3
+    projectID : : String : identifiant du projet GNS3
+
+    Returns
+    -------
+    routeur.console_host : nom du routeur sur le projet GNS3
+    routeur.console : numéro de port dur routeur sur GNS3
+    Ces paramètres permettent de se connecter à chaques routeur via telnet
+    """
+    server = Gns3Connector("http://localhost:3080")
+    lab = Project(name=projectName, connector=server)
+    lab.get()
+    print(projectID)
+    routeur = Node(project_id=projectID, name= nameRouteur, connector=server)
+    routeur.get()
+    return routeur.console_host, routeur.console
+    
+
+def configRouteur(routeur,data,cible):
+    """
+    Parameters
+    ----------
+    routeur : String : Numéro du routeur.
+    data : Dictionnaire : Contient les paramètres de configuration de chaques routeurs
+    cible : String : Indique quel élement on configure (internalRouting, bgp, interface, all)
+    -------
+    Fonction permettant de configurer un routeur via telnet.
+    """
+    conf = data[routeur] # dictionaire contenant la configuration du routeur 
+    name = int(routeur)
+    nameRouteur ="R"
+    nameRouteur += str(name)
+    #Récupération du nom et du numéro de port du routeur
+    projectID = data["1"]["projectId"]
+    projectName =  data["1"]["projectName"]
+    nom, port = portRouteur(nameRouteur,projectName, projectID)
+    
+    #Lecture des commandes de configuration des routeurs 
+    with open("commande.json", 'r') as fcommande :
+        commande = json.load(fcommande) #dictionaire contenant les commandes de configuration
+   
+    try:
+        with telnetlib.Telnet(nom, port , timeout=30) as tn: 
+            print(f"connexion établie avec R{name} ")
+            tn.write(b"\r")
+            tn.read_until(b"#")
+            tn.write(b"configure terminal\r")
+            tn.read_until(b"#")
+            tn.write(b"no logging console\r")
+            tn.read_until(b"#")
+            tn.write(b"end\r")
+            tn.read_until(b"#")
+            
+            if cible == "internalRouting" :
+                ### Configuration du protocol de routage interne ####
+                libgns3.internalRoutingProtocol(name,conf,commande,tn)
+                time.sleep(1)
+                
+            elif cible =="interface":
+                #### Configuratioon des interfaces ###
+                libgns3.interfaceConfig(name, conf, commande,tn)
+                time.sleep(1)
+                
+            elif cible == "bgp":
+                ### Configuration de bgp ###
+                if conf['border'] == "True" :
+                    libgns3.bgpConfig (name, conf, commande,tn)
+                    time.sleep(1)
+                    libgns3.setCommunity(name, conf, commande, tn)
+                    time.sleep(1)
+                    libgns3.filterCommunity(name, conf, commande, tn)
+          
+          
+            else :
+                #libgns3.internalRoutingProtocol(name,conf,commande,tn)
+                time.sleep(1)
+                libgns3.interfaceConfig(name, conf, commande,tn)
+                time.sleep(1)
+                libgns3.labelProtocolConfig(name, conf, commande,tn)
+                time.sleep(1)
+                libgns3.bgpConfig (name, conf, commande,tn)
+                time.sleep(1)
+                libgns3.ipv4BgpConfig(name, conf, commande,tn)
+                
+                
+                
+                #if conf['border'] == "True" :
+                    #libgns3.setCommunity(name, conf, commande, tn)
+                    #time.sleep(1)
+                    #libgns3.filterCommunity(name, conf, commande, tn)
+                    #time.sleep(1)
+                    
+    except ConnectionRefusedError:
+        print(f"La connexion à R{name} a été refusée.")
+    except Exception as e:
+        print(f"Erreur de connexion a R{name}: {e}")
+    
+def resetRouteur(routeur,data,cible):
+    """
+    Parameters
+    ----------
+    routeur : String : Numéro du routeur.
+    data : Dictionnaire : Contient les paramètres de configuration de chaques routeurs
+    cible : String : Indique quel élement on réinitialise (internalRouting, bgp, interface, all)
+    -------
+    Fonction permettant d'effacer la configuration d'un routeur via telnet.
+    """
+    conf = data[routeur] # dictioanaire contenant la configuration du routeur 
+    name = int(routeur)
+    nameRouteur ="R"
+    nameRouteur += str(name)
+    
+    #Récupération du nom et du numéro de port du routeur
+    projectID = data["1"]["projectId"]
+    projectName =  data["1"]["projectName"]
+    nom, port = portRouteur(nameRouteur,projectName, projectID)
+    
+    #Lecture des commandes de configuration 
+    with open("commande.json", 'r') as fcommande :
+        commande = json.load(fcommande)
+    
+    try:
+        with telnetlib.Telnet(nom, port , timeout=30) as tn: #nom et numero de port
+            print(f"connexion établie avec R{name} ")
+            tn.read_until(b"#")
+            
+            if cible == "internalRouting" :
+                ### Suppression du protocol de routage interne ####
+                libgns3.noInternalRoutingProtocol(name,conf,commande,tn)
+                time.sleep(1)
+                
+            elif cible =="interface":
+                #### Suppression de la configuration sur les interfaces (adrresse ip notamment) ###
+                libgns3.noInterfaceConfig(name, conf, commande,tn)
+                time.sleep(1)
+                
+            elif cible == "bgp":
+                if conf['border'] == "True" :
+                ### Suppression du protocol bgp ###
+                    libgns3.noBgpConfig (name, conf, commande,tn)
+                    time.sleep(1)
+                    libgns3.resetCommunity(name, conf, commande, tn)
+                    time.sleep(1)
+                    libgns3.resetFilterCommunity(name, conf, commande, tn)
+                    time.sleep(1)
+               
+            else :
+                #libgns3.noInternalRoutingProtocol(name,conf,commande,tn)
+                time.sleep(1)
+                libgns3.noInterfaceConfig(name, conf, commande,tn)
+                time.sleep(1)
+                libgns3.noLabelProtocolConfig(name, conf, commande,tn)
+                time.sleep(1)
+                
+                if conf['border'] == "True" :
+                    #libgns3.noBgpConfig (name, conf, commande,tn)
+                    time.sleep(1)
+                    #libgns3.resetCommunity(name, conf, commande, tn)
+                    time.sleep(1)
+                    #libgns3.resetFilterCommunity(name, conf, commande, tn)
+                    time.sleep(1)
+                
+    except ConnectionRefusedError:
+        print(f"La connexion à R{name} a été refusée.")
+    except Exception as e:
+        print(f"Erreur de connexion a R{name}: {e}")
+        
+        
+        
+def clearLastConfig() :
+       """
+        Fonction permettant de vider les fichiers contenant la dernière config
+        de chaques routeurs.
+       """
+       with open("DicoRouteur2.json", 'r') as fichier:
+           data = json.load(fichier)
+       for routeur in data.keys() :
+           with open(f"lastConfig/lastConfig{routeur}.json","w") as fichier:
+               lastConfig = {}
+               json.dump(lastConfig, fichier)
+
+def main():
+    with open("DicoRouteur2.json", 'r') as fichier:
+        data = json.load(fichier)
+
+    routeur = input("Quel est le numéro du routeur que vous voulez configurer. Si vous voulez tout configurer : all ?")
+    while routeur not in data.keys() and routeur !="all" :
+        routeur = input("Quel est le numéro du routeur que vous voulez configurer. Si vous voulez tout configurer : all ?")
+
+    action = input("Reset ou config?\n")
+    cible =input("Quelle est votre cible : internalRouting, interface, bgp, all\n")
+    if action == "config" :
+        if routeur == "all" :
+            for routeur in data.keys() :
+                configRouteur(routeur,data,cible)
+        else : 
+            configRouteur(routeur,data,cible)
+    else :
+        if routeur == "all" :
+            for routeur in data.keys() :
+                resetRouteur(routeur,data,cible)
+        else : 
+            resetRouteur(routeur,data,cible)
+     
+if __name__ == "__main__" :
+    answer = input("Voulez vous effacer les fichiers contenant la dernière config [oui/non] ?")
+    if answer == "oui" :
+        clearLastConfig()
+    main()
+    
+    
+    
